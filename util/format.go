6a4da6bcf3f46e1654e4b0429e520097f70aa459
kbirk <birk.kevin@gmail.com>
2015-11-18 23:27:05 -0500

    Moved byte unit formatting to util package.

---
+++ util/format.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+    "fmt"
+)
+
+// powers of 1000 and their suffixes
+var suffixes = map[uint8]string {
+    0: "B",
+    1: "KB",
+    2: "MB",
+    3: "GB",
+    4: "TB",
+    5: "PB",
+    6: "EB",
+}
+
+func formatRecursive( size float64, powerOfThousand uint8 ) string {
+    if ( size > 1000 ) {
+        return formatRecursive( size / 1000, powerOfThousand + 1 )
+    }
+    return fmt.Sprintf( "%.2f " + suffixes[powerOfThousand], size )
+}
+
+func FormatBytes( bytes float64 ) string {
+    return formatRecursive( bytes, 0 )
+}
