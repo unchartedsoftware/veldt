7e312423730e137e2254347971bb8703ed48ffa6
kbirk <birk.kevin@gmail.com>
2015-11-21 01:50:15 -0500

    Added exists function to store API, added implmentation for redis store.

--- store/redis.go
+++ store/redis.go
@@ -1,19 +1,17 @@
 package store
 
 import (
-	"fmt"
-
 	"github.com/garyburd/redigo/redis"
 )
 
 var redisPool *redis.Pool
 var redisHost = "localhost"
 var redisPort = "6379"
-var maxConnections = 10
+var maxConnections = 32
 
-func getConnection() *redis.Pool {
-    if redisPoll == nil {
-        redisPool := redis.NewPool( func() ( redis.Conn, error ) {
+func getConnection() redis.Conn {
+    if redisPool == nil {
+        redisPool = redis.NewPool( func() ( redis.Conn, error ) {
     		conn, err := redis.Dial( "tcp", redisHost + ":" + redisPort )
     		if err != nil {
     			return nil, err
@@ -28,12 +26,20 @@ func getConnection() *redis.Pool {
 func RedisGet( key string ) ( []byte, error ) {
     conn := getConnection()
     defer conn.Close()
-    return redis.String( c.Do( "GET", key ) )
+    return redis.Bytes( conn.Do( "GET", key ) )
 }
 
 // RedisSet will store a byte slice under a given key in redis.
 func RedisSet( key string, value []byte ) error {
     conn := getConnection()
     defer conn.Close()
-    status, err := conn.Do( "SET", key, value )
+    _, err := conn.Do( "SET", key, value )
+	return err
+}
+
+// RedisExists returns whether or not a key exists in redis.
+func RedisExists( key string ) ( bool, error ) {
+    conn := getConnection()
+    defer conn.Close()
+    return redis.Bool( conn.Do( "Exists", key ) )
 }
--- store/store.go
+++ store/store.go
@@ -1,10 +1,14 @@
 package store
 
-type getFromStore func( key string ) ( []byte, error )
 type setInStore func( key string, value []byte ) error
+type getFromStore func( key string ) ( []byte, error )
+type existsInStore func( key string ) ( bool, error )
 
 // Get returns a value from the store for a given string key.
 var Get getFromStore = RedisGet
 
 // Set will store a byte slice under a given key in the store.
 var Set setInStore = RedisSet
+
+// Exists returns whether or not a key exists in the store.
+var Exists existsInStore = RedisExists
