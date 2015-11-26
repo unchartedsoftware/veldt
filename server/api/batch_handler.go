bd0399dcc2e347db69e513687083dd0e7e209cfb
kbirk <birk.kevin@gmail.com>
2015-11-26 17:52:09 -0500

    Initial refactor of batch_handler. Error handling not entirely complete.

--- server/api/batch_handler.go
+++ server/api/batch_handler.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"net/http"
-	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -21,44 +20,104 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: maxMessageSize,
 }
 
-func writeMessage(conn *websocket.Conn, mutex *sync.Mutex, tileReq *tiling.TileRequest) {
-	p, err := tiling.GetTile(tileReq)
+// TileDispatcher represents a single clients tile dispatcher.
+type TileDispatcher struct {
+	Chan chan *tiling.TileResponse
+	Conn *websocket.Conn
+}
+
+// NewTileDispatcher returns a pointer to a new tile dispatcher object.
+func NewTileDispatcher(w http.ResponseWriter, r *http.Request) (*TileDispatcher, error) {
+	// open a websocket connection
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, err
+	}
+	// set the message read limit
+	conn.SetReadLimit(maxMessageSize)
+	dispatcher := &TileDispatcher{
+		Chan: make(chan *tiling.TileResponse),
+		Conn: conn,
+	}
+	// set the dispatcher to listen for any responses to its dispatched requests,
+	// writing them out into the websocket connection as they come
+	go dispatcher.ListenForResponses()
+	// return the dispatcher
+	return dispatcher, nil
+}
+
+// GetRequest waits on the websocket connection for tile requests.
+func (t *TileDispatcher) GetRequest() (*tiling.TileRequest, error) {
+	// tile request
+	tileReq := &tiling.TileRequest{}
+	// wait on read
+	err := t.Conn.ReadJSON(&tileReq)
+	if err != nil {
+		return nil, err
+	}
+	return tileReq, nil
+}
+
+// func (t* TileDispatcher) Listen() (*tiling.TileRequest, error) {
+// 	select {
+// 	case err := <- t.ErrChan:
+// 		return nil, err
+// 	case req := <- t.ReqChan:
+// 		return req, nil
+// 	}
+// }
+
+// DispatchRequest takes a tile request and dispatches it to the generation package.
+func (t *TileDispatcher) DispatchRequest(tileReq *tiling.TileRequest) {
+	promise, err := tiling.GetTile(tileReq)
 	if err != nil {
 		log.Warn(err)
 	}
-	p.OnComplete(func(res interface{}) {
-		// cast to tile response
-		tileRes := res.(*tiling.TileResponse)
-		// writes are not thread-safe
-		mutex.Lock()
-		conn.SetWriteDeadline(time.Now().Add(writeWait))
-		err = conn.WriteJSON(tileRes)
-		mutex.Unlock()
-		if err != nil {
-			log.Warn(err)
-		}
+	promise.OnComplete(func(res interface{}) {
+		// cast to tile response and pass to response channel
+		t.Chan <- res.(*tiling.TileResponse)
 	})
 }
 
+// ListenForResponses waits on tile responses and communicates them to the client via websocket.
+func (t *TileDispatcher) ListenForResponses() {
+	for resp := range t.Chan {
+		// write response to websocket
+		t.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+		t.Conn.WriteJSON(resp)
+		// err := t.Conn.WriteJSON(resp)
+		// if err != nil {
+		// 	t.ErrChan <- err
+		// }
+	}
+}
+
+// Close closes the dispatchers internal channel and websocket connection.
+func (t *TileDispatcher) Close() {
+	// close response channel
+	close(t.Chan)
+	// close websocket connection
+	t.Conn.Close()
+}
+
 func batchHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+	// create dispatcher
+	dispatcher, err := NewTileDispatcher(w, r)
 	if err != nil {
 		log.Warn(err)
 		return
 	}
-	defer conn.Close()
-	conn.SetReadLimit(maxMessageSize)
-	mutex := &sync.Mutex{}
+	// begin read pump
 	for {
-		// tile request
-		tileReq := &tiling.TileRequest{}
-		// wait on read
-		err := conn.ReadJSON(&tileReq)
+		// wait on tile request
+		tileReq, err := dispatcher.GetRequest()
 		if err != nil {
 			log.Debug(err)
 			break
 		}
-		// execute tile request
-		go writeMessage(conn, mutex, tileReq)
+		// dispatch the tile request
+		dispatcher.DispatchRequest(tileReq)
 	}
+	// clean up dispatcher internals
+	dispatcher.Close()
 }
