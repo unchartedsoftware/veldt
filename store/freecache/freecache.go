13b7b86d4daa1e7c40d3f0f4cd6797197eb33fef
kbirk <birk.kevin@gmail.com>
2016-04-14 13:21:31 -0400

    Added freecache store implementation.

--- store/freecache/freecache.go
+++ store/freecache/freecache.go
@@ -1,7 +1,6 @@
 package freecache
 
 import (
-	"fmt"
 	"runtime"
 	"sync"
 
@@ -12,7 +11,7 @@ import (
 )
 
 var (
-	mutex = sync.Mutex{}
+	mutex  = sync.Mutex{}
 	caches = make(map[int]*freecache.Cache)
 )
 
@@ -28,13 +27,6 @@ func getCache(byteSize int) *freecache.Cache {
 	if !ok {
 		log.Infof("Creating freecache instance of size `%d` bytes", byteSize)
 		cache = freecache.NewCache(byteSize)
-		cache.Set([]byte("derp:derp:derp"), []byte("test_data"), 0)
-		got, err := cache.Get([]byte("derp:derp:derp"))
-		if err != nil {
-		    fmt.Println(err)
-		} else {
-		    fmt.Println(string(got))
-		}
 		caches[byteSize] = cache
 	}
 	mutex.Unlock()
@@ -46,39 +38,32 @@ func getCache(byteSize int) *freecache.Cache {
 func NewConnection(byteSize int, expirySeconds int) store.ConnectionConstructor {
 	return func() (store.Connection, error) {
 		return &Connection{
-			cache: getCache(byteSize),
+			cache:  getCache(byteSize),
 			expiry: expirySeconds,
 		}, nil
 	}
 }
 
-// Get when given a string key will return a byte slice of data from redis.
+// Get when given a string key will return a byte slice of data from freecache.
 func (r *Connection) Get(key string) ([]byte, error) {
-	fmt.Printf("Get key: %s\n", key)
-	fmt.Printf("Get key: %v\n", []byte(key))
- 	return r.cache.Get([]byte(key))
+	return r.cache.Get([]byte(key))
 }
 
-// Set will store a byte slice under a given key in redis.
+// Set will store a byte slice under a given key in freecache.
 func (r *Connection) Set(key string, value []byte) error {
-	fmt.Printf("Set key: %s\n", key)
-	fmt.Printf("Set key: %v\n", []byte(key))
 	return r.cache.Set([]byte(key), value, r.expiry)
 }
 
-// Exists returns whether or not a key exists in redis.
+// Exists returns whether or not a key exists in freecache.
 func (r *Connection) Exists(key string) (bool, error) {
-	fmt.Printf("Exists key: %s\n", key)
-	fmt.Printf("Exists key: %v\n", []byte(key))
 	_, err := r.cache.Get([]byte(key))
 	if err != nil {
-		fmt.Printf("Exists err: %v\n", err)
 		return false, nil
 	}
 	return true, nil
 }
 
-// Close closes the redis connection.
+// Close closes the freecache connection.
 func (r *Connection) Close() {
-	r.cache.Clear()
+	// no-op
 }
