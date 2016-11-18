800edea222f1b5ab59d880b169cf379bcf44fda0
Philippe Horne <phorne@uncharted.software>
2016-11-18 16:10:33 -0500

    Changed has Citus query from contains to overlap to match elastic behaviour.

--- generation/citus/has.go
+++ generation/citus/has.go
@@ -19,7 +19,7 @@ func NewHas() (prism.Query, error) {
 // Get adds the parameters to the query and returns the string representation.
 func (q *Has) Get(query *Query) (string, error) {
 	// Check that the array contains the values.
-	// Use the column @> ARRAY[value1, value2] notation.
+	// Use the column && ARRAY[value1, value2] notation.
 	clause := ""
 
 	//Generate the array values.
@@ -29,6 +29,6 @@ func (q *Has) Get(query *Query) (string, error) {
 	}
 
 	//Remove the leading ", " from the array contents.
-	clause = fmt.Sprintf("%s @> ARRAY[%s]", q.Field, clause[2:])
+	clause = fmt.Sprintf("%s && ARRAY[%s]", q.Field, clause[2:])
 	return clause, nil
 }
