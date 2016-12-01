ac321505eed4c9e154058f7c3cf15fa601f0aefe
kbirk <birk.kevin@gmail.com>
2016-12-01 14:00:45 -0500

    Fix non-lod encoding stride.

--- tile/encode.go
+++ tile/encode.go
@@ -6,15 +6,15 @@ import (
 )
 
 func Encode(points []float32) []byte {
-	bytes := make([]byte, len(points)*8)
+	bytes := make([]byte, len(points)*4)
 	for i := 0; i < len(points); i += 2 {
 		x := points[i]
 		y := points[i+1]
 		binary.LittleEndian.PutUint32(
-			bytes[i*8:i*8+4],
+			bytes[i*4:i*4+4],
 			math.Float32bits(x))
 		binary.LittleEndian.PutUint32(
-			bytes[i*8+4:i*8+8],
+			bytes[i*4+4:i*4+8],
 			math.Float32bits(y))
 	}
 	return bytes
