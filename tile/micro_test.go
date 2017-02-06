82400c0c8b758c4b8896c28bb1f0118b10c18731
asumila <alexei.sumila@gmail.com>
2017-02-06 18:10:45 +0100

    additional test for micro

--- tile/micro_test.go
+++ tile/micro_test.go
@@ -13,6 +13,13 @@ var _ = Describe("Micro", func() {
 	params := make(map[string]interface{})
 	params["lod"] = 1
 
+	encd := make([]map[string]interface{},2)
+	hit1 := make(map[string]interface{})
+	hit2 := make(map[string]interface{})
+	encd[0] = hit1
+	encd[1] = hit2
+	encd_rslt := []byte(`{"hits":null,"offsets":[0,8,8,8],"points":[1,1]}`)
+
 	It("should set LOD", func() {
 		ok := mcr.Parse(params)
 		Expect(ok).To(BeNil())
@@ -24,5 +31,12 @@ var _ = Describe("Micro", func() {
 		result := mcr.ParseIncludes([]string{"a", "b"}, "a", "b")
 		Expect(result).To(Equal([]string{"a", "b"}))
 	})
+
+	It("should set parse includes correctly", func() {
+		result, ok := mcr.Encode(encd, []float32{1,1})
+		Expect(ok).To(BeNil());
+		Expect(result).To(Equal(encd_rslt))
+	})
+
 
 })
