971f86a9600b116d7f17d889fb52078065ef19dc
kbirk <birk.kevin@gmail.com>
2017-02-07 14:39:39 -0500

    More coverage to macro test.

--- tile/macro_test.go
+++ tile/macro_test.go
@@ -1,6 +1,8 @@
 package tile_test
 
 import (
+	"math"
+
 	"github.com/unchartedsoftware/veldt/tile"
 
 	. "github.com/onsi/ginkgo"
@@ -11,9 +13,17 @@ import (
 var _ = Describe("Macro", func() {
 
 	var macro *tile.Macro
+	var input []float32
 
 	BeforeEach(func() {
 		macro = &tile.Macro{}
+		input = []float32{
+			137.24, 7.07, 224.49, 123.95,
+			124.51, 148.33, 72.40, 22.15,
+			160.13, 77.59, 128.77, 183.32,
+			65.36, 36.25, 107.91, 250.01,
+			96.05, 198.40, 66.70, 73.39,
+		}
 	})
 
 	Describe("Parse", func() {
@@ -25,6 +35,39 @@ var _ = Describe("Macro", func() {
 			err := macro.Parse(params)
 			Expect(err).To(BeNil())
 			Expect(macro.LOD).To(Equal(4))
+		})
+	})
+
+	Describe("Encode", func() {
+		It("should not encode offset information if LOD == 0", func() {
+			bytesPerFloat := 4
+			params := JSON(
+				`{
+					"lod": 0
+				}`)
+			err := macro.Parse(params)
+			Expect(err).To(BeNil())
+			bytes, err := macro.Encode(input)
+			Expect(err).To(BeNil())
+			Expect(len(bytes)).To(Equal(len(input) * bytesPerFloat))
+		})
+		It("should encode offset information if LOD > 0", func() {
+			bytesPerFloat := 4
+			bytesPerOffset := 4
+			params := JSON(
+				`{
+					"lod": 4
+				}`)
+			err := macro.Parse(params)
+			Expect(err).To(BeNil())
+			bytes, err := macro.Encode(input)
+			pointCount := bytesPerFloat
+			offsetCount := bytesPerOffset
+			pointsBytes := len(input) * bytesPerFloat
+			offsetBytes := int(math.Pow(4, 4)) * bytesPerOffset // lod = 4
+			totalBytes := pointCount + offsetCount + pointsBytes + offsetBytes
+			Expect(err).To(BeNil())
+			Expect(len(bytes)).To(Equal(totalBytes))
 		})
 	})
 })
