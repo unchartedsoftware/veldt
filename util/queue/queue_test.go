fa367b9d71d82f2b175b4b0e9548a42cca5d27ed
kbirk <birk.kevin@gmail.com>
2017-02-08 14:05:44 -0500

    Code coverage for queue tests.

--- util/queue/queue_test.go
+++ util/queue/queue_test.go
@@ -2,6 +2,7 @@ package queue_test
 
 import (
 	"sync"
+	"time"
 
 	"github.com/unchartedsoftware/veldt/util/queue"
 
@@ -9,50 +10,107 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-type testRequest struct {
+type countRequest struct {
 	count int
 	mu    *sync.Mutex
 }
 
-func newTestRequest() *testRequest {
-	return &testRequest{
+func newTestRequest() *countRequest {
+	return &countRequest{
 		mu: &sync.Mutex{},
 	}
 }
 
-func (r *testRequest) Create() ([]byte, error) {
+func (r *countRequest) Create() ([]byte, error) {
 	r.mu.Lock()
 	r.count++
 	r.mu.Unlock()
 	return nil, nil
 }
 
-func (r *testRequest) Decrement() {
+func (r *countRequest) Decrement() {
 	r.mu.Lock()
 	r.count--
 	r.mu.Unlock()
 }
 
-func (r *testRequest) Count() int {
+func (r *countRequest) Count() int {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	return r.count
 }
 
+type sleepRequest struct {
+}
+
+func newSleepRequest() *sleepRequest {
+	return &sleepRequest{}
+}
+
+func (r *sleepRequest) Create() ([]byte, error) {
+	time.Sleep(time.Millisecond * 200)
+	return nil, nil
+}
+
 var _ = Describe("Queue", func() {
 
 	var q *queue.Queue
+	var m int
 
 	BeforeEach(func() {
 		q = queue.NewQueue()
+		m = 256
 	})
 
 	Describe("Send", func() {
 
-		It("should never execute more than max concurrent at the same time", func() {
+		It("should execute the requests when there is availability", func() {
+			req := newTestRequest()
+			for i := 0; i < m; i++ {
+				_, err := q.Send(req)
+				Expect(err).To(BeNil())
+			}
+			count := req.Count()
+			Expect(count).To(Equal(m))
+		})
+
+	})
+
+	Describe("SetMaxConcurrent", func() {
+
+		It("should set the maximum number of concurrent requests", func() {
 			req := newTestRequest()
 			n := 8
-			m := 256
+			q.SetMaxConcurrent(n)
+			for i := 0; i < m; i++ {
+				_, err := q.Send(req)
+				Expect(err).To(BeNil())
+				count := req.Count()
+				Expect(count).To(BeNumerically("<=", n))
+				req.Decrement()
+			}
+			count := req.Count()
+			Expect(count).To(Equal(0))
+		})
+
+		It("should handle setting above the current max number", func() {
+			req := newTestRequest()
+			n := 64
+			q.SetMaxConcurrent(n)
+			for i := 0; i < m; i++ {
+				_, err := q.Send(req)
+				Expect(err).To(BeNil())
+				count := req.Count()
+				Expect(count).To(BeNumerically("<=", n))
+				req.Decrement()
+			}
+			count := req.Count()
+			Expect(count).To(Equal(0))
+		})
+
+		It("should handle setting below the current max number", func() {
+			req := newTestRequest()
+			n := 8
 			q.SetMaxConcurrent(n)
 			for i := 0; i < m; i++ {
 				_, err := q.Send(req)
@@ -72,9 +130,23 @@ var _ = Describe("Queue", func() {
 		It("should set the queue length", func() {
 			n := 20
 			q.SetLength(n)
-			for i := 0; i < n; i++ {
-				q.Send(newTestRequest())
+			var err error
+			c := make(chan error)
+			for i := 0; i < m; i++ {
+				select {
+				case err = <-c:
+					break
+				default:
+					go func() {
+						_, err = q.Send(newSleepRequest())
+						if err != nil {
+							c <- err
+						}
+					}()
+				}
+
 			}
+			Expect(err).ToNot(BeNil())
 		})
 
 	})
