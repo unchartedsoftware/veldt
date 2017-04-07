2ae5107ec87083434ed7b5971a49411acc91b093
Nathan Kronenfeld <nicole.oresme@gmail.com>
2017-04-07 13:37:18 -0400

    Convert unit tests to run under ginkgo

--- generation/salt/config_test.go
+++ generation/salt/config_test.go
@@ -1,54 +1,43 @@
 package salt
 
 import (
-	"reflect"
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-func assertEquals(t *testing.T, name string, expected, actual interface{}) bool {
-	if expected != actual {
-		var eType = reflect.TypeOf(expected)
-		var aType = reflect.TypeOf(actual)
-		t.Errorf("%s: Expected %v[%v], got %v[%v]", name, expected, eType, actual, aType)
-		return false
-	}
-	return true
-}
-func TestFullSaltConfig(t *testing.T) {
-	config, err := ReadConfig("testdata/salt-configuration-full.conf")
-	if err != nil {
-		t.Error(err)
-	}
+var _ = Describe("Salt configuration", func() {
+	It("should read all properties correctly from a test config file", func() {
+		config, err := ReadConfig("testdata/salt-configuration-full.conf")
+		Expect(err).To(BeNil())
 
-	assertEquals(t, "Host", "rabbitmq.uncharted.software", config.host)
-	assertEquals(t, "Port", int64(1234), config.port)
-	assertEquals(t, "Queue", "salt-test-queue", config.serverQueue)
-	assertEquals(t, "Queues", 3, len(config.queueConfigs))
-	assertEquals(t, "Queue bunny name", "bunny-queue", config.queueConfigs["bunny"].queue)
-	assertEquals(t, "Queue bunny durability", false, config.queueConfigs["bunny"].durable)
-	assertEquals(t, "Queue bunny deletability", true, config.queueConfigs["bunny"].deletable)
-	assertEquals(t, "Queue bunny exclusivity", false, config.queueConfigs["bunny"].exclusive)
-	assertEquals(t, "Queue bunny no-wait", true, config.queueConfigs["bunny"].noWait)
-	assertEquals(t, "Queue lapin name", "lapin-queue", config.queueConfigs["lapin"].queue)
-	assertEquals(t, "Queue lapin durability", true, config.queueConfigs["lapin"].durable)
-	assertEquals(t, "Queue lapin deletability", true, config.queueConfigs["lapin"].deletable)
-	assertEquals(t, "Queue lapin exclusivity", false, config.queueConfigs["lapin"].exclusive)
-	assertEquals(t, "Queue lapin no-wait", false, config.queueConfigs["lapin"].noWait)
-	assertEquals(t, "Queue hare name", "hare-queue", config.queueConfigs["hare"].queue)
-	assertEquals(t, "Queue hare durability", false, config.queueConfigs["hare"].durable)
-	assertEquals(t, "Queue hare deletability", false, config.queueConfigs["hare"].deletable)
-	assertEquals(t, "Queue hare exclusivity", true, config.queueConfigs["hare"].exclusive)
-	assertEquals(t, "Queue hare no-wait", true, config.queueConfigs["hare"].noWait)
-}
+		Expect(config.host).To(Equal("rabbitmq.uncharted.software"), "Host parameter")
+		Expect(config.port).To(Equal(int64(1234)), "Port")
+		Expect(config.serverQueue).To(Equal("salt-test-queue"), "Queue")
+		Expect(len(config.queueConfigs)).To(Equal(3), "Queues")
+		Expect(config.queueConfigs["bunny"].queue).To(Equal("bunny-queue"), "Queue bunny name")
+		Expect(config.queueConfigs["bunny"].durable).To(Equal(false), "Queue bunny durability")
+		Expect(config.queueConfigs["bunny"].deletable).To(Equal(true), "Queue bunny deletability")
+		Expect(config.queueConfigs["bunny"].exclusive).To(Equal(false), "Queue bunny exclusivity")
+		Expect(config.queueConfigs["bunny"].noWait).To(Equal(true), "Queue bunny no-wait")
+		Expect(config.queueConfigs["lapin"].queue).To(Equal("lapin-queue"), "Queue lapin name")
+		Expect(config.queueConfigs["lapin"].durable).To(Equal(true), "Queue lapin durability")
+		Expect(config.queueConfigs["lapin"].deletable).To(Equal(true), "Queue lapin deletability")
+		Expect(config.queueConfigs["lapin"].exclusive).To(Equal(false), "Queue lapin exclusivity")
+		Expect(config.queueConfigs["lapin"].noWait).To(Equal(false), "Queue lapin no-wait")
+		Expect(config.queueConfigs["hare"].queue).To(Equal("hare-queue"), "Queue hare name")
+		Expect(config.queueConfigs["hare"].durable).To(Equal(false), "Queue hare durability")
+		Expect(config.queueConfigs["hare"].deletable).To(Equal(false), "Queue hare deletability")
+		Expect(config.queueConfigs["hare"].exclusive).To(Equal(true), "Queue hare exclusivity")
+		Expect(config.queueConfigs["hare"].noWait).To(Equal(true), "Queue hare no-wait")
+	})
 
-func TestDefaultSaltConfig(t *testing.T) {
-	config, err := ReadConfig("testdata/salt-configuration-empty.conf")
-	if err != nil {
-		t.Error(err)
-	}
+	It("Should default all configuration parameters properly", func() {
+		config, err := ReadConfig("testdata/salt-configuration-empty.conf")
+		Expect(err).To(BeNil())
 
-	assertEquals(t, "Host", "localhost", config.host)
-	assertEquals(t, "Port", int64(5672), config.port)
-	assertEquals(t, "Queue", "salt", config.serverQueue)
-	assertEquals(t, "Queues", 0, len(config.queueConfigs))
-}
+		Expect(config.host).To(Equal("localhost"), "Host")
+		Expect(config.port).To(Equal(int64(5672)), "Port")
+		Expect(config.serverQueue).To(Equal("salt"), "Queue")
+		Expect(len(config.queueConfigs)).To(Equal(0), "Queues")
+	})
+})
